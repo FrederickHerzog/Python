102cc80873110fe6831e473d20048826ace7ea49
Frederick Herzog <51469035+FrederickHerzog@users.noreply.github.com>
2019-06-07 14:45:23 -0500

    Create Buttons.py

---
+++ code_snippets/GUI/tkinter/Buttons.py
@@ -0,0 +1,29 @@
+#Frederick Herzog
+
+import tkinter as tk 
+import datetime 
+
+HEIGHT = 300
+WIDTH = 400
+
+def datee():
+    print(datetime.datetime.now())
+
+def hello():
+	print("Hello")
+
+window = tk.Tk()
+
+canvas = tk.Canvas(window, height=HEIGHT, width=WIDTH)
+canvas.pack()
+
+frame = tk.Frame(window, bg='#81F79F')
+frame.place(relwidth=1, relheight=1)
+
+
+btn1 = tk.Button(frame, text="Say Hi", bg='gray', fg='#FA5882', command=hello)
+btn1.place(relx=.375, rely=.35, relwidth=.25, relheight=.15)
+btn2 = tk.Button(frame, text="Date And Time", command=datee)
+btn2.place(relx=.35, rely=.5, relwidth=.30, relheight=.15)
+
+window.mainloop()
