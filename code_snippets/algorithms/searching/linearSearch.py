39e5431bc91e77ec833eeba936b068909f0c6d0d
RickHerzog <51469035+flh4@users.noreply.github.com>
2020-06-04 14:14:46 -0500

    Update linearSearch.py

--- code_snippets/algorithms/searching/linearSearch.py
+++ code_snippets/algorithms/searching/linearSearch.py
@@ -1,1 +1,12 @@
 #Frederick Herzog
+
+def lin_search(l, x):
+  for i in range(len(l)):
+    if l[i] == x:
+      return "Target "+str(x)+" was found at index: "+str(i)
+  return "Target "+str(x)+" not found.
+
+a = [100, 20, 14, 7, 4, 12, 18, 24, 54, 99, 37, 8]
+target = 24
+
+lin_search(a, target)
