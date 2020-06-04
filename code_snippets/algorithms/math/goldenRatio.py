180064446c91b0d6d0e552b7e087ac5d012d1b25
RickHerzog <51469035+flh4@users.noreply.github.com>
2020-06-04 14:50:39 -0500

    Update goldenRatio.py

--- code_snippets/algorithms/math/goldenRatio.py
+++ code_snippets/algorithms/math/goldenRatio.py
@@ -2,21 +2,19 @@
 # algorithm to estimate the golden
 # ratio using the fibonacci sequence
 
-def fibs(n):
-    fibs = []
-    a = 0
-    for i in range (1, n+1):
-        current = a + i
-        fibs.append(current)
-        a += 1
-    return fibs
+def golden(n):
+        a, b = 0, 1
+        print ("~~~~~~~~~~~~")
+        print ("Golden Mean:")
+        print ("~~~~~~~~~~~~")
+	
+        while a < n:
+                a, b = b, a + b
+                s = a / b
+                print (s)
 
-def goldRatio(l):
-    pass
+        return fibs
 
-#test
-x = fibs(20)
-print(x)
-
+golden(500)
         
     
