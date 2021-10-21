0066da605cfe8c138c0fa0f26609bb2579f9e12c
flh4 <door3@protonmail.com>
2021-10-21 18:07:05 -0500

    Update

---
+++ code_snippets/re/reg.py
@@ -0,0 +1,28 @@
+import re                                                                                                                                                                                                                                           
+
+
+def AreDatesValid(dates):
+    #YYYY/MM/DD
+    regex = "^\d{4}/(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])$"
+    d = []
+    for date in dates:
+        r = re.match(regex, date)
+        if r:
+            d.append(date)
+        else: 
+            print()
+            print(date + "  is not a valid date.,,,\n")
+    return d
+
+
+def main():
+    dates_list = ["2020/10/05", "2021-09-21", "1999/03/03", "1987/12/12", "1885/02/01", '95-02-10', "20210510"]
+    valid_dates = AreDatesValid(dates_list)
+    print("******************************")
+    print()
+    print("These are valid:")
+    print("----------------")
+    for i, date in enumerate(valid_dates): print(i+1, date)
+    print()
+
+main()
